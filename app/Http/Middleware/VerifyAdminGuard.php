4214ee5c9468b9b060d7e052a4ca3e4f547fd1da
enzojunqueiraa <enzobjunqueira@gmail.com>
2024-02-08 15:05:48 -0300

    autentificacao

---
+++ app/Http/Middleware/VerifyAdminGuard.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Admin;
+use Closure;
+use Illuminate\Http\Client\ResponseSequence;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class VerifyAdminGuard
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {   
+        if(!Auth::user() instanceof Admin){
+            return response()->json([
+                'status'=> false,
+                'message'=> 'Não é uma instancia de ADM'
+            ]);
+        }
+
+        return $next($request);
+    }
+}
